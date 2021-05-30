b2bae674e371253b46b356bb62c62a901347d9bc
Rajat Shukla <rajats.think@gmail.com>
2021-05-30 21:10:54 +0530

    fixed bugs in check functionality

--- todo-list/js/app2.js
+++ todo-list/js/app2.js
@@ -7,29 +7,27 @@ function submitForm(){
         let li = document.createElement("li");
         li.innerHTML = `
         <span id="todo-item" class = "todo-class" >${todo.value}</span>
-        <button name="checkButton" id="check"><i class="fas fa-check-square"></i></button>
+        <button name="checkButton" id="check" onclick="checkFunction(event)"><i class="fas fa-check-square"></i></button>
         <button name="deleteButton" id="delete"><i class="fas fa-trash"></i></button>
         `;
         li.classList.add('todo-list-item');
         let ul = document.querySelector("ul");
         ul.appendChild(li);
         todo.value = '';
-        checkFunction();
     }
 }
 
 submitButton.addEventListener("click", submitForm);
 
-function checkFunction(){
-    event.preventDefault();
+function checkFunction(e){
+    let checkButton = e.target;
     console.log("checkFunction");
-    var checkButton = document.getElementById("check"); 
-    checkButton.addEventListener("click", lineThroughFunc);
+    checkButton.addEventListener("click", lineThroughFunc(e, checkButton));
 }
 
-function lineThroughFunc(){
+function lineThroughFunc(e, checkButton){
     console.log("lineThroughFunc");
-    var spanVal = document.getElementById("todo-item");
+    let spanVal = e.target.parentNode;
     spanVal.classList.toggle("done");
 }
 
