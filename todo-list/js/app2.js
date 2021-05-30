f66caf58bfbeffd768c19efbb5cc3203fd26de30
Rajat Shukla <rajats.think@gmail.com>
2021-05-30 19:43:26 +0530

    Used arrow functions ES6

--- todo-list/js/app2.js
+++ todo-list/js/app2.js
@@ -33,3 +33,49 @@ function lineThroughFunc(){
     spanVal.classList.toggle("done");
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+// let submitButton = document.getElementById("submitBtn");
+// let todo = document.getElementById("inp");
+
+// const submitForm = () => {
+//     event.preventDefault();
+//     if(todo.value != '') {
+//         let li = document.createElement("li");
+//         li.innerHTML = `
+//         <span id="todo-item" class = "todo-class" >${todo.value}</span>
+//         <button name="checkButton" id="check"><i class="fas fa-check-square"></i></button>
+//         <button name="deleteButton" id="delete"><i class="fas fa-trash"></i></button>
+//         `;
+//         li.classList.add('todo-list-item');
+//         let ul = document.querySelector("ul");
+//         ul.appendChild(li);
+//         todo.value = '';
+//         checkFunction();
+//     }    
+// }
+
+// submitButton.addEventListener("click", submitForm);
+
+// const checkFunction = () => {
+//     event.preventDefault();
+//     console.log("checkFunction");
+//     let checkButton = document.getElementById("check"); 
+//     checkButton.addEventListener("click", lineThroughFunc);    
+// }
+
+// const lineThroughFunc = () => {
+//     console.log("lineThroughFunc");
+//     let spanVal = document.getElementById("todo-item");
+//     spanVal.classList.toggle("done");
+// }
